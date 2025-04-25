b7a1047bef26c901ba9bfc3f641eb5ba8f55e860
Ninosaurus0611 <nino.daniele@ziggo.nl>
2025-04-25 14:18:18 +0200

    oefenen

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-Name = input("Enter your name: ")
-Age = input("Enter your age: ")
-Height = input("Enter your height: ")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height: "))
 
-print("I", Name, "am", Age, "years old, and I'm", Height, "meters tall")
+print("I", name, "am", age, "years old, and I'm", height, "meters tall.")
+
+if age >= 50:
+    print("Ik ben kapot oud")
+elif age >= 18:
+    print("Ik ben volwassen")
+else:
+    print("Ik ben een kind")
+
+if height >= 1.80:
+    print("Valid lengte")
+else:
+    print("Invalid lengte")
